31cee088dec284c72df666a4f46519f078cc8210
XinchenZhang <54343729+XinchenZhang@users.noreply.github.com>
2022-03-20 23:47:40 -0700

    Update Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -7,8 +7,37 @@ import           GHC.IO.Encoding
 main :: IO ()                             
 main = do
   setLocaleEncoding utf8
-  error "TBD: main" 
+  putStrLn(welcome) 
+  helper 0 Nano.prelude
 
+helper :: Int -> Nano.Env -> IO()
+helper x env = do
+    putStrFlush ( printf "\x03BB [%d] " x)
+    c <- getLine
+    case strCmd c of
+      CQuit -> do 
+              doQuit
+
+      CEval input -> do 
+                    doEval env input
+                    helper (x + 1) env
+
+      CRun input -> do 
+                  doRun input
+                  helper (x + 1) env
+
+      CLoad input -> do
+                    env <- doLoad input
+                    putStrLn("definitions:" ++ (help env)) 
+                    helper (x + 1) env
+                      where 
+                        help :: Nano.Env -> String
+                        help [] = "" 
+                        help ((x1, x2) : r) = " " ++ x1 ++ (help r)
+                                   
+      CUnknown -> do 
+                doUnknown
+                helper (x + 1) env
 
 --------------------------------------------------------------------------------
 -- | Some useful functions 
@@ -16,5 +45,6 @@ main = do
 -- putStr   :: String -> IO ()
 -- hFlush   :: 
 -- putStrLn :: String -> IO ()
+
 -- getLine  :: IO String 
 
